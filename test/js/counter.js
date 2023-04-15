f5b2c2c7e4d1ed6e995bf3bdbc9e829c3705348d
divyesh1707 <divyeshramoliya707@gmail.com>
2023-04-15 09:20:16 +0530

    15-4

---
+++ test/js/counter.js
@@ -0,0 +1,19 @@
+
+let x = 0;
+
+document.getElementById("x").innerHTML = x;
+
+function decrement(){
+    x = x-1;
+    document.getElementById("count").innerHTML = x;
+    console.log(x);
+
+}
+
+function increment(){
+    x = x + 1;
+    document.getElementById("count").innerHTML = x;
+    
+    console.log(y);
+
+}
